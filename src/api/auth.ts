393530f5ed32be3178002af4038e4ed0935e418a
Taras <taraskravets95@gmail.com>
2024-01-16 13:54:36 +0200

    fix: fixed CORS to sign-in

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,15 +1,20 @@
 import axios from 'axios';
 
 import { endpoints } from './endpoints';
+import { SignInArgs } from './types';
 
 export const _signUp = async (data: any) => {
     const response = await axios.post(endpoints.auth.signUp, data);
     return response.data; // as IUser;
 };
 
-export const _signIn = async (data: any) => {
-    const response = await axios.post(endpoints.auth.signIn, data);
-    return response.data; // as IUser;
+export const _signIn = async (data: SignInArgs) => {
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    const response = await axios.post(endpoints.auth.signIn, data, { headers });
+    return response.data;
 };
 
 export const _signInGoogle = () => async () => {
