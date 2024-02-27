ee4cfc432f6c12726e75dcc67812b576d572879b
Taras <taraskravets95@gmail.com>
2024-02-27 18:51:57 +0200

    feat(epic-mode): added epic-mod to about-us page

--- src/components/sections/AboutUs/AboutUsView.tsx
+++ src/components/sections/AboutUs/AboutUsView.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Document } from '@contentful/rich-text-types';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import styles from './AboutUsView.module.scss';
+import { Button } from '@/components/common/Button/Button';
 
 //--------------------------------------------------------
 
@@ -15,5 +16,54 @@ interface AboutUsProps {
 //--------------------------------------------------------
 
 export const AboutUsView: React.FC<AboutUsProps> = ({ content }) => {
-    return <section className={styles.section}>{documentToReactComponents(content)}</section>;
+    const [epic, setEpic] = useState<boolean>(false);
+
+    const handleSwitchOffEpicMode = () => setEpic(false);
+    const handleSwitchOnEpicMode = () => setEpic(true);
+
+    useEffect(() => {
+        let timerId: NodeJS.Timeout | null = null;
+
+        const handleKeyDown = (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                handleSwitchOffEpicMode();
+            }
+        };
+
+        if (epic) {
+            document.body.style.backgroundColor = '#141414';
+            document.body.style.overflow = 'hidden';
+            timerId = setTimeout(() => {
+                handleSwitchOffEpicMode();
+            }, 24000);
+
+            document.addEventListener('keydown', handleKeyDown);
+        }
+
+        return () => {
+            if (timerId) {
+                clearTimeout(timerId);
+            }
+            document.body.style.backgroundColor = '#fff';
+            document.body.style.overflow = 'auto';
+
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [epic]);
+
+    console.log(epic);
+
+    return (
+        <section className={epic ? styles['epic-mode'] : styles.section}>
+            {epic ? (
+                <>
+                    <Button label="Skip" onClick={handleSwitchOffEpicMode} />
+                    <div className={styles['epic-content']}>{documentToReactComponents(content)}</div>
+                </>
+            ) : (
+                <>{documentToReactComponents(content)}</>
+            )}
+            {!epic && <Button label="Do Epic" onClick={handleSwitchOnEpicMode} />}
+        </section>
+    );
 };
